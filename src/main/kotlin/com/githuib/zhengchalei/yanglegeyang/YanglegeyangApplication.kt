ccd7c6a10b4501b52783272cd6dfe1428b1a96f3
zhengchalei <stone981023@gmail.com>
2022-09-18 17:48:18 +0800

    Initial commit

---
+++ src/main/kotlin/com/githuib/zhengchalei/yanglegeyang/YanglegeyangApplication.kt
@@ -0,0 +1,11 @@
+package com.githuib.zhengchalei.yanglegeyang
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YanglegeyangApplication
+
+fun main(args: Array<String>) {
+	runApplication<YanglegeyangApplication>(*args)
+}
---
+++ src/test/kotlin/com/githuib/zhengchalei/yanglegeyang/YanglegeyangApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.githuib.zhengchalei.yanglegeyang
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YanglegeyangApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
